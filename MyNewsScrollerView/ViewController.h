b6518e59646973643d85860c5bfd310c88a55454
410445919@qq.com <410445919@qq.com>
2016-07-18 13:44:44 +0800

    Initial Commit

---
+++ MyNewsScrollerView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyNewsScrollerView
+//
+//  Created by yang on 16/7/18.
+//  Copyright © 2016年 poplary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyNewsScrollerView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyNewsScrollerView
+//
+//  Created by yang on 16/7/18.
+//  Copyright © 2016年 poplary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
